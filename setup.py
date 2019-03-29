467576d0bce74ac165bbdd31b632c21b2770d1e6
sbesda <sergio.besada@gmail.com>
2019-03-29 21:47:23 +0100

    DES Final Version 1.0.0

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pymep",
+    version="1.0.0",
+    author="Sergio Besada",
+    author_email="sergio.besada@gmail.com",
+    description="Python Math Expression Parser",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sbesada/MathParserLibrary",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
