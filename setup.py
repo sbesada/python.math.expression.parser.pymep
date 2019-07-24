6d38185335e695b2e6790f2da624c4b79d65b9ce
sbesda <sergio.besada@gmail.com>
2019-07-24 17:46:35 +0200

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pymep",
-    version="1.0.0",
+    version="1.0.2",
     author="Sergio Besada",
     author_email="sergio.besada@gmail.com",
     description="Python Math Expression Parser",
